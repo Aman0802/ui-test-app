37b359d6f4cbce4038c8dc88182bed9e681cea95
Aman0802 <aman.khubani@gmail.com>
2020-12-30 16:17:48 +0530

    resolved the redirect bug

--- src/modules/Admin/index.js
+++ src/modules/Admin/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Switch, Route, useRouteMatch, Redirect } from "react-router-dom";
+import { Switch, Route, useRouteMatch, Link } from "react-router-dom";
 import LandingPage from "../../components/LandingPage";
 import { Login } from "./Auth";
 
@@ -8,10 +8,10 @@ export default function AdminModule() {
   const match = useRouteMatch();
   return (
     <>
-      <Redirect to="/home"> Home </Redirect>
+      <Link to="/home"> Home </Link>
+      <Link to="/login"> Login </Link>
 
       <Switch>
-        <Route exact path={`${match.path}`} component={LandingPage} />
         <Route exact path={`${match.path}home`} component={LandingPage} />
         <Route exact path={`${match.path}login`} component={Login} />
       </Switch>
